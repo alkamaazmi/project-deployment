5ac26a5b030482da3d20ba87df9f4c191d6c7163
Alkama Azmi <alkamaazmi07@gmail.com>
2023-03-18 12:22:07 +0530

    some commit

--- server.js
+++ server.js
@@ -27,8 +27,6 @@ connectDb();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(bodyParser.json({extended:true}));
-app.use(bodyParser.urlencoded({extended:true}));
 // app.use(express.static(path.join(__dirname,'./client/build')))
 //routes
 app.use("/api/v1/auth", authRoute);
